9c1e8f8c642a5c52263dc45bb9ddd270dff0e3f3
Stepan <sfermin2007@icloud.com>
2025-10-02 16:58:09 +0200

    Initial Commit

---
+++ Bubble Orbit/Bubble_OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_OrbitApp.swift
+//  Bubble Orbit
+//
+//  Created by Stepan Yarikova on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bubble OrbitTests/Bubble_OrbitTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_OrbitTests.swift
+//  Bubble OrbitTests
+//
+//  Created by Stepan Yarikova on 2/10/25.
+//
+
+import Testing
+@testable import Bubble_Orbit
+
+struct Bubble_OrbitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
